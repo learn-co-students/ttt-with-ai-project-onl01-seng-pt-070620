4292fc59fc4b7401d38e624e5c4c245b01f42ee8
Jill Short <jillian.b.short@gmail.com>
2020-08-14 20:40:31 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2
   
   WIN_COMBINATIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -7,8 +7,10 @@ class Game
     [0, 4, 8], [2, 4, 6]
   ]
   
-  def initialize(player_1, player_2, board)
-    
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
 end
