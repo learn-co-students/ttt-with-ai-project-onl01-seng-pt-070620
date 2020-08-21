24350c7f6a68cec405377d12d8d1e7b62b2b95ef
kinnari <kinnari1022@yahoo.com>
2020-08-21 14:30:47 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -13,9 +13,12 @@ class Board
   end
 
   def display
-    binding.pry
-    board = Board.new
-    puts board.cells
+ 
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
   
  
