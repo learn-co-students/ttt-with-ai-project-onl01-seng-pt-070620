b8caff610c36c805c50db7a46a95107b9c605900
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-08-26 12:21:54 -0700

    player.rb

---
+++ lib/player.rb
@@ -0,0 +1,31 @@
+class Player
+
+    attr_reader :token
+
+    def initialize(token)
+        # self.token = token
+        @token = token
+    end 
+
+    # def token=(token)
+    #     if @token != nil
+    #         raise NoMethodError #< Exception
+    #         self.respond_to?(@token)
+    #     end 
+    # end 
+
+
+    # does not work
+    # begin
+    #     @token != nil
+    # rescue NoMethodError < Exception
+    #     raise NoMethodError
+    # end 
+
+    # def token
+    #     if @token != nil
+    #         raise NoMethodError
+    #     end 
+    # end 
+
+end 
