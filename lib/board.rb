891758240c2b24da9795db6848b82357b6b304df
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-08-26 15:48:55 -0700

    game file

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 class Board
-    attr_accessor :cells
+    
+    attr_accessor :cells, :game
     # using instance variable to store object board values
     @cells = []
 
--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
 
-    attr_reader :token
+    attr_reader :token, :game
 
     def initialize(token)
         # self.token = token
