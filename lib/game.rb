49787fe6a76923fe667dd310f1e2c4e6f071a4ad
julieconnors <juliana.g.connors@gmail.com>
2021-05-23 05:34:00 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -18,4 +18,49 @@ class Game
         @player_2 = player_2
     end
 
+    def current_player
+        @board.turn_count % 2 == 0 ? player_1 : player_2
+    end
+
+    def position_taken?(index)
+        if @board.cells[index] == " " || @board.cells[index] == "" || @board.cells[index] == nil
+          false  
+        elsif @board.cells[index] == "X" || @board.cells[index] == "O"
+          true
+        end
+    end
+
+    def won?
+        # returns true for draw or win
+        # returns false for in progress game
+        WIN_COMBINATIONS.detect do |win_combination|
+            # binding.pry
+            @board.cells[win_combination[0]] == @board.cells[win_combination[1]] && @board.cells[win_combination[1]] == @board.cells[win_combination[2]] && position_taken?(win_combination[0])
+        end
+    end
+
+    def draw?
+        if self.full? && !self.won?
+            true
+        elsif self.won?
+            false
+        elsif !self.full? && !self.won?
+            false
+        end
+    end
+
+    def full?
+        @board.cells.all? do |block|
+            if block == "X" || block == "O"
+                true
+            else
+                false
+            end
+        end
+    end
+
+    def over?
+        self.draw? || self.won?
+    end
+
 end
