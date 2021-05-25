0974a79342a7e50d779dcd9b4f9ff04a790e1651
julieconnors <juliana.g.connors@gmail.com>
2021-05-25 06:10:34 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,34 @@
 module Players
     class Computer < Player
-        def move
+        def move(board)
+            # binding.pry
+            if !board.full?
+                #binding.pry
+                best_move(board)
+            end
+        end
+
+        def best_move(board)
+            if board.cells.empty?
+                binding.pry
+                return "4"
+            elsif board.turn_count == 1
+                return "0"
+            elsif board.turn_count == 2
+                return "6"
+            elsif board.turn_count == 3
+                return "2"
+            elsif board.turn_count == 4
+                return "1"
+            elsif board.turn_count == 5
+                return "7"
+            elsif board.turn_count == 6
+                return "5"
+            elsif board.turn_count == 7
+                return "3"
+            else 
+                return "8"
+            end
         end
     end
 end
